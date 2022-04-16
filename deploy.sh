1d1acef8c4dc27f12c1a64c12102109eecdb9e0b
Fran Vallejo <neotherack@gmail.com>
2022-04-16 17:50:48 +0200

    improved deployer script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 quasar build
+sudo rm -f /var/www/menu_barluna/js/*
+sudo rm -f /var/www/menu_barluna/css/*
 sudo cp -r dist/spa/* /var/www/menu_barluna/
 sudo chown -R www-data. /var/www/menu_barluna/
